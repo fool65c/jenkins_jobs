76e55befff7f9db4f35f3ea4e1912bee770e371e
Kevin Mager <kmager@MagerBB.local>
2017-12-22 14:50:27 -0500

    pipeline test

--- simpleJob.groovy
+++ simpleJob.groovy
@@ -2,3 +2,16 @@ def myJob = freeStyleJob('SimpleJob')
 myJob.with {
     description 'A Simple Job'
 }
+
+buildPipelineView("TEST_Pipeline") {
+  filterBuildQueue()
+  filterExecutors()
+  title('TEST Pipeline')
+  displayedBuilds(30)
+  selectedJob(myJob.name)
+  alwaysAllowManualTrigger()
+  showPipelineParameters()
+  refreshFrequency(60)
+  startsWithParameters()
+  showPipelineParametersInHeaders()
+}
